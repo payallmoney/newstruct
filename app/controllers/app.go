7381eb074967b792265baaf65eb5b7a37fa7cae9
payallmoney <payallmoney@163.com>
2014-03-13 19:32:18 +0800

    init

---
+++ app/controllers/app.go
@@ -0,0 +1,12 @@
+package controllers
+
+import "github.com/robfig/revel"
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	greeting := "Aloha World"
+	return c.Render(greeting)
+}
